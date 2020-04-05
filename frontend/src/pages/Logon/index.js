020d513891081d56fec517bfec46ba0d57fdecef
felixtitonina <felixtitonina@outlook.com>
2020-04-04 22:10:29 -0300

    front do app pronto

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom' //evita que a pag seja recarregada 
+import React, { useState } from 'react'
+import { Link, useHistory} from 'react-router-dom' //evita que a pag seja recarregada 
 import { FiLogIn } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css'
 
@@ -8,16 +10,35 @@ import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
 export default function Logon() {
+    const [id, setId] = useState('')
+    const history = useHistory()
+
+    async function handleLogin(e) {
+        e.preventDefault()
+        try {
+            const response = await api.post('sessions', { id })
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.name)
+            history.push('/profile')
+        } catch (error) {
+             alert('Fala no login, tente novamente')
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu login</h1>
-                    <input placeholder="Sua ID" />
+                    <input
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
                     <Link className="back-link" to="/register">
-                        <FiLogIn size={16} color="#E02041"/>
+                        <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
                     </Link>
                 </form>
